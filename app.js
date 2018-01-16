3b630ed3092f887ccbb071b7bba4fa27b22eb60f
Connor Kirkwood <cdkirkwood@gmail.com>
2018-01-16 11:48:53 -0500

    setting up twitter bank

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const nunjucks = require('nunjucks');
 
-app.use(morgan("combined"))
-console.log('hello');
+var locals = {
+  title: 'An Example',
+  people: [
+      { name: 'Gandalf'},
+      { name: 'Frodo' },
+      { name: 'Hermione'}
+  ]
+};
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+app.use(morgan("combined"));
 
 // app.use(function(req, res, next){
 // 	console.log(req.method + " " + req.path);
@@ -13,7 +25,7 @@ console.log('hello');
 
 
 
-app.get("/", (req, res, next) => res.send("Hello World"));
+app.get("/", (req, res, next) => res.render('index.html', locals));
 
 
 
