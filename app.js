5cb0fbb9bacfbfbf4aee7079fe69a0bf39188d10
Marko Lisonek <mlisonek98@gmail.com>
2018-01-16 10:34:56 -0500

    yp to git collaboration

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan("combined"))
+
+// app.use(function(req, res, next){
+// 	console.log(req.method + " " + req.path);
+// 	console.log(res.statusCode);
+// 	next();
+// })
+
+
+
+app.get("/", (req, res, next) => res.send("Hello World"));
+
+
+
+app.listen(3000, () => console.log('server listening'))
+
