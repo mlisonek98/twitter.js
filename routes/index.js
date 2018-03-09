b0e7784631b33dcfa286c0b0b250a43dcb3fa266
Connor Kirkwood <cdkirkwood@gmail.com>
2018-03-09 11:00:58 -0500

    push it

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,33 @@ const express = require('express');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets, showForm: true } );
 });
+
+const Eventbrite = require('eventbrite');
+const eb_client = Eventbrite({
+  'app_key': "ITATE5HGD4DYT4IB6MJQIB67ZALROSPVXVI64JFKECOXRJ3VVV",
+  'user_key': "AWXQ6FOPFWLQBPAIORNZ"
+});
+
+var params = { 'city': "San Francisco", 'region': "CA" };
+
+router.get('/event', (req, res, next) => {
+    eb_client.event_search(params, function (err, data) {
+      console.log(err);
+      console.log(data);
+    });
+    next()
+  })
+
 router.get('/style', function(req,res){
  res.sendFile('stylesheets/style.css')
 })
